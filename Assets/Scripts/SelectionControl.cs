4e0ce06189e00c59d514cb16ce7af959149061fd
dgarcia202 <wft2000@gmail.com>
2018-01-13 15:16:26 +0100

    basic object selection

---
+++ Assets/Scripts/SelectionControl.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SelectionControl : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown(0)){
+			RaycastHit hit;
+			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+
+			// var select = GameObject.FindWithTag("select").transform;
+			if (Physics.Raycast (ray, out hit, 100f)){
+				// select.tag = "none";
+				// hit.collider.transform.tag = "select";
+
+				Debug.Log ("+++ selected obj " + hit.collider.gameObject.ToString());
+			}
+		}	
+	}
+}
